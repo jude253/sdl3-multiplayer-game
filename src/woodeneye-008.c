b6b4ae6c6c9a255f5a92cee4bf0bdc8a51747870
Jude <judecapachietti@gmail.com>
2025-03-01 21:46:26 -0800

    Add mongoose library

--- src/woodeneye-008.c
+++ src/woodeneye-008.c
@@ -5,6 +5,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <mongoose.h>
 
 /*
 Quesion: what does MAP_BOX_SCALE do?  I change it, recompile the game 
