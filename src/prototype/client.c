e523046591f7924590e0e5bb018826bb0c4623f4
Jude <judecapachietti@gmail.com>
2025-03-15 11:45:34 -0700

    Add rendering of random colors from example

--- src/prototype/client.c
+++ src/prototype/client.c
@@ -2,6 +2,26 @@
 
 static char debug_string[32];
 
+static void draw(SDL_Renderer *renderer)
+{
+    const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
+    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
+    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
+
+    /* clear the window to the draw color. */
+    SDL_RenderClear(renderer);
+
+    /* Draw FPS */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDebugText(renderer, 5, 5, debug_string);
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(renderer);
+}
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     AppState *as = SDL_calloc(1, sizeof(AppState));
@@ -47,11 +67,13 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     Uint64 now = SDL_GetTicksNS();
     Uint64 dt_ns = now - past;
 
+    draw(as->renderer);
     /*
     Not exactly sure what this does, but thinking its to prevent issues
-    with roll over. 
+    with roll over.
     */
-    if (now - last > 999999999) {
+    if (now - last > 999999999)
+    {
         last = now;
         SDL_snprintf(debug_string, sizeof(debug_string), "%" SDL_PRIu64 " fps", accu);
         accu = 0;
--- src/prototype/server.c
+++ src/prototype/server.c
@@ -2,6 +2,26 @@
 
 static char debug_string[32];
 
+static void draw(SDL_Renderer *renderer)
+{
+    const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
+    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
+    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
+
+    /* clear the window to the draw color. */
+    SDL_RenderClear(renderer);
+
+    /* Draw FPS */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDebugText(renderer, 5, 5, debug_string);
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(renderer);
+}
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     AppState *as = SDL_calloc(1, sizeof(AppState));
@@ -46,6 +66,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     static Uint64 past = 0;
     Uint64 now = SDL_GetTicksNS();
     Uint64 dt_ns = now - past;
+
+    draw(as->renderer);
 
     /*
     Not exactly sure what this does, but thinking its to prevent issues
